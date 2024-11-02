35207ee118223033c0b3466e1bc9bb92e6be735e
vovakirdan <vova.kirdan@mail.ru>
2024-11-02 17:02:27 +0300

    passphrase

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import asyncssh  # For asynchronous SSH connections
 
 if not os.path.isfile(os.getenv('SSH_PRIVATE_KEY_PATH')):
     raise FileNotFoundError(f"SSH private key file not found at {os.getenv('SSH_PRIVATE_KEY_PATH')}")
-key = asyncssh.read_private_key(os.getenv('SSH_PRIVATE_KEY_PATH'))
+key = asyncssh.read_private_key(os.getenv('SSH_PRIVATE_KEY_PATH'), passphrase=os.getenv('PASSPHRASE'))
 
 # Enable logging
 logging.basicConfig(
--- testssh.py
+++ testssh.py
@@ -7,4 +7,4 @@ load_dotenv()
 if not Path(os.getenv('SSH_PRIVATE_KEY_PATH')).exists():
     raise FileNotFoundError(f"SSH private key file not found at {os.getenv('SSH_PRIVATE_KEY_PATH')}")
 
-key = asyncssh.read_private_key(os.getenv('SSH_PRIVATE_KEY_PATH'))
+key = asyncssh.read_private_key(os.getenv('SSH_PRIVATE_KEY_PATH'), passphrase=os.getenv('PASSPHRASE'))
